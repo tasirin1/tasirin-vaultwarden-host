7640df22829eefd138fcce5c54c4447d63297da5
tasirin1 <tasirin1@users.noreply.github.com>
2026-08-05 01:32:22 +0000

    fix: tambah import IOException (compile gagal di Gradle)

--- app/src/main/java/com/vaultwarden/android/MainActivity.java
+++ app/src/main/java/com/vaultwarden/android/MainActivity.java
@@ -27,6 +27,7 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
